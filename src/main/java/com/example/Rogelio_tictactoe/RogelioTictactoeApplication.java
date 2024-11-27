70111069141852b89641e0d1db2ee78f279b9c6b
Rogerpervaz <ulisescodi@gmail.com>
2024-11-26 23:48:09 -0600

    New project

---
+++ src/main/java/com/example/Rogelio_tictactoe/RogelioTictactoeApplication.java
@@ -0,0 +1,13 @@
+package com.example.Rogelio_tictactoe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RogelioTictactoeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RogelioTictactoeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Rogelio_tictactoe/RogelioTictactoeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Rogelio_tictactoe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RogelioTictactoeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
